1675d9411c4a33cb9f67c2bacdc8a767ef66aead
Kazuki Iwata <iwacchi.kz@gmail.com>
2020-02-02 22:33:25 +0900

    ストレージ情報表示機能追加

---
+++ playcanvas-setting.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+        SCRIPT_PREFIX = "";
+        SCENE_PATH = "840324.json";
+        CONTEXT_OPTIONS = {
+            'antialias': true,
+            'alpha': false,
+            'preserveDrawingBuffer': false,
+            'preferWebGl2': true
+        };
+        SCRIPTS = [ 25657573 ];
+        CONFIG_FILENAME = "config.json";
+        INPUT_SETTINGS = {
+            useKeyboard: true,
+            useMouse: true,
+            useGamepads: false,
+            useTouch: true
+        };
+        pc.script.legacy = false;
+        PRELOAD_MODULES = [
+            {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/26590392/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/26590390/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/26590391/1/ammo.js', 'preload' : true},
+        ];
